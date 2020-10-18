538dcb764bee3f56d07930b28ac0a9125eedc5c7
xpedroleonardo <aispedroleonardo@gmail.com>
2020-10-18 13:29:41 -0300

    Alterando configurações

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://192.168.15.39:3333/uploads/${image.path}`
+      url: `http://SEU_ENDEREÇO_IP:3333/uploads/${image.path}`
     }
   },
   renderMany(images: Image[]) {
